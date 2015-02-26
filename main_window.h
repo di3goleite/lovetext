1ac5e20f76d67580b371605ef4ab44a20e1205d6
FelipeFS <felipefsdev@gmail.com>
2015-02-26 13:17:27 -0300

    Update on the preferences window and how the application handler pointer is passed.

--- main_window.h
+++ main_window.h
@@ -81,7 +81,7 @@ struct cwindow_handler
 };
 
 void initialize_lua(struct cwindow_handler *window_handler, struct cpreferences *preferences);
-struct cwindow_handler *alloc_window_handler(struct cpreferences *preferences);
+struct cwindow_handler *alloc_window_handler(struct capplication_handler *application_handler, struct cpreferences *preferences);
 struct cbuffer_ref *create_page(struct cwindow_handler *window_handler, gchar *file_name, gboolean modfied, gchar *text, struct cpreferences *preferences);
 
 #endif
